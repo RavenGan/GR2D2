c9fff9a58be11d5af3ba94cf73837da44bd1a677
RavenGan <46891773+RavenGan@users.noreply.github.com>
2022-07-12 19:39:57 -0400

    Add files via upload

--- GR2D2_fcn.R
+++ GR2D2_fcn.R
@@ -9,22 +9,24 @@ library(statmod)
 library(GIGrvg)
 library(Matrix)
 library(Rcpp)
-library(Rfast)
+# library(Rfast)
 library(pracma)
+library(matrixcalc) # Check positive definiteness
+library(matlib)
 
 #  Sampling for graphical R2D2
-GR2D2_E <- function(S, n, burnin, nmc, eps = 1e-20){
+GR2D2_E <- function(S, n, burnin, nmc, eps = 1e-7){
   # Input:
   #   S = Y'*Y: sample covariance matrix * n
   #   n: sample size
   #   burnin, nmc: number of MCMC burnins and saved samples
-
+  
   # Output:
   #   omega_save: (p by p by nmc) matrices of saved posterior samples of precision matrix.
   #   psi_save: (p by p by nmc) vector of saved samples of psi
   #   phi_save: (p by p by nmc) vector of saved samples of phi (local tuning parameter)
   #   w_save: (1 by nmc) vector of saved samples of w (global tuning parameter)
-
+  
   p <- nrow(S)
   print(p)
   omega_save <- array(numeric(), c(p, p, 0))
@@ -39,7 +41,7 @@ GR2D2_E <- function(S, n, burnin, nmc, eps = 1e-20){
   C = 1
   p_n = p*(p-1)/2
   alpha_pi = C/((p_n^(b/2))*(sample_size^(r*b/2))*log(sample_size))
-
+  
   a = alpha_pi*p*(p-1)/2
   #   Initial values 2
   Omega <- diag(p)
@@ -48,29 +50,71 @@ GR2D2_E <- function(S, n, burnin, nmc, eps = 1e-20){
   Phi <- matrix(1, nrow = p, ncol = p)
   w <- 1
   xi <- 1
-
+  
   # Main iterations
   for (iter in 1: (burnin + nmc)) {
+    print(iter)
     for (i in 1:p){
       # sample gamma
       s_ii <- S[i, i]
       gamma <- rgamma(1, shape = sample_size/2+1, rate = s_ii/2)
+      
+      # Value adjustment for Sigma
+      Sigma[which(Sigma > 1/eps)] <- 1/eps  # New line------------------------
+      Sigma[which(Sigma < -1/eps)] <- -1/eps  # New line------------------------
+      Sigma[which(Sigma < eps & Sigma > 0)] <- eps  # New line------------------------
+      Sigma[which(Sigma > -eps & Sigma < 0)] <- -eps  # New line------------------------
+      
       sigma_vec_ii <- as.matrix(Sigma[-i, i])
       inverse_Omega_ii <- Sigma[-i, -i] - sigma_vec_ii %*% t(sigma_vec_ii)/Sigma[i, i]
+      
       # sample beta
       Lambda <- Psi*Phi
+      
+      Lambda[which(Lambda > 1/eps)] <- 1/eps  # New line------------------------
+      Lambda[which(Lambda < -1/eps)] <- -1/eps  # New line------------------------
+      Lambda[which(Lambda < eps & Lambda > 0)] <- eps  # New line------------------------
+      Lambda[which(Lambda > -eps & Lambda < 0)] <- -eps  # New line------------------------
+      
       lambda_vec_ii <- Lambda[-i, i]
       Lambda_star <- diag(lambda_vec_ii)
       D_inv <- s_ii*inverse_Omega_ii + 2*solve(w*Lambda_star, tol = 1e-10000)
+      
+      # Value adjustment for D_inv
+      # D_inv[which(D_inv > 1/eps)] <- 1/eps  # New line------------------------
+      # D_inv[which(D_inv < eps)] <- eps  # New line------------------------
+
+      if (!is.positive.definite(D_inv)){ # New line------------------------
+        D_inv_PD <- nearPD(D_inv)
+        D_inv <- D_inv_PD$mat %>% as.matrix()
+      }
+      
       D_inv.chol <- chol(D_inv)
+      
+      D_inv[which(D_inv > 1/eps)] <- 1/eps  # New line------------------------
+      D_inv[which(D_inv < eps & D_inv > 0)] <- eps  # New line------------------------
+      D_inv[which(D_inv > -eps & D_inv < 0)] <- -eps  # New line------------------------
+      D_inv[which(D_inv < -1/eps )] <- -1/eps  # New line------------------------
+      
       mu_i <- -mldivide(D_inv, S[-i, i])
-
-      beta_vec <- mu_i+ mldivide(D_inv.chol, mvrnorm(n = 1, rep(0, p-1), diag(p-1)))
-
+      
+      # Value adjustment for D_inv
+      # mu_i <- -solve(D_inv, tol = 1e-10000)%*%S[-i, i] # New line------------------------
+      
+      # mu_i <- -Ginv(D_inv, tol = eps)%*%S[-i, i] # Use generalized inverse------------------------
+      
+      D_inv.chol[which(D_inv.chol > 1/eps)] <- 1/eps  #s New line------------------------
+      D_inv.chol[which(D_inv.chol < -1/eps)] <- -1/eps  #s New line------------------------
+      D_inv.chol[which(D_inv.chol < eps & D_inv.chol > 0)] <- eps  # New line------------------------
+      D_inv.chol[which(D_inv.chol > -eps & D_inv.chol < 0)] <- -eps  # New line------------------------
+      
+      beta_vec <- mu_i +  mldivide(D_inv.chol, mvrnorm(n = 1, rep(0, p-1), diag(p-1)))
+      # beta_vec <- mu_i + Ginv(D_inv.chol, tol = eps)%*%mvrnorm(n = 1, rep(0, p-1), diag(p-1)) # Use generalized inverse------------------------
+      
       Omega[i, i] <- gamma + t(beta_vec)%*%inverse_Omega_ii%*%beta_vec
       Omega[-i, i] <- beta_vec
       Omega[i, -i] <- beta_vec
-
+      
       # update Psi
       for (row in 1:p) {
         Psi[row, i] <- rinvgauss(1, mean = sqrt(Phi[row, i]*w/2)/abs(Omega[row, i]), shape = 1)
@@ -78,7 +122,7 @@ GR2D2_E <- function(S, n, burnin, nmc, eps = 1e-20){
         Psi[i, row] <- 1/Psi[row, i]
       }
       diag(Psi) <- 1
-
+      
       # update Sigma
       Sigma[-i, -i] <- inverse_Omega_ii + (inverse_Omega_ii%*%beta_vec)%*%t(inverse_Omega_ii%*%beta_vec)/gamma
       Sigma[-i, i] <- -(inverse_Omega_ii%*%beta_vec)/gamma
@@ -92,7 +136,11 @@ GR2D2_E <- function(S, n, burnin, nmc, eps = 1e-20){
     lambda_mn <- alpha_pi - 1/2
     
     tem <- 2*(Omega)^(2)/Psi
-    tem[which(tem == 0)] <- eps
+    tem[which(tem > 1/eps)] <- 1/eps # New lines----------
+    tem[which(tem < -1/eps)] <- -1/eps # New lines----------
+    tem[which(tem < eps & tem > 0)] <- eps # New lines----------
+    tem[which(tem > -eps & tem < 0)] <- -eps # New lines----------
+    
     for (m in 1:(p-1)) {
       for (n in (m+1):p) {
         Chi_mn <- tem[m, n]
@@ -103,7 +151,7 @@ GR2D2_E <- function(S, n, burnin, nmc, eps = 1e-20){
       }
     }
     Phi <- diag(p) + T_summary/sum_T
-
+    
     # sample w
     Chi_w <- 0
     for (m in 1:(p-1)) {
@@ -113,11 +161,18 @@ GR2D2_E <- function(S, n, burnin, nmc, eps = 1e-20){
     }
     psi_w <- 2*xi
     lambda_w <- a - p*(p-1)/4
+    
+    if(Chi_w > 1/eps) { # New lines----------
+      Chi_w <- 1/eps
+    } else {
+      Chi_w <- Chi_w
+    }
+    
     w <- rgig(n = 1, lambda = lambda_w, chi = Chi_w, psi = psi_w)
-
+    
     # sample xi
     xi <- rgamma(1, shape = (a + b), rate = w+1)
-
+    
     # save Omega, psi, phi, w
     if (iter > burnin){
       omega_save <- abind(omega_save, Omega)
@@ -131,7 +186,7 @@ GR2D2_E <- function(S, n, burnin, nmc, eps = 1e-20){
 
 
 # Record observations based on credible intervals
-adjusted_fcn <- function(p, estimate, nmc, CI_p){
+adjusted_fcn <- function(p, estimate, nmc, CI_p = 0.95){
   adjusted <- matrix(0, nrow = p, ncol = p)
   lower_p <- (1-CI_p)/2
   upper_p <- 1-lower_p
